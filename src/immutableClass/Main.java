d69bb619047edb2bd29d6b41df6a234ca3c7b90f
Thomas Meyer <thomasmmeyer88@gmail.com>
2021-07-11 15:24:50 -0500

    comp string length

--- src/immutableClass/Main.java
+++ src/immutableClass/Main.java
@@ -1,8 +1,29 @@
 package immutableClass;
 
+import immutableClass.immutableString;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Test");
+		
+		immutableString s1 = new immutableString("abcd");
+		immutableString s2 = new immutableString("bcda");
+		immutableString s3 = new immutableString("cdab");
+		immutableString s4 = new immutableString("random");
+		
+		boolean comp1 = s1.compareImmutableStrings(s1, s2);
+		boolean comp2 = s2.compareImmutableStrings(s1, s3);
+		boolean comp3 = s2.compareImmutableStrings(s1, s4);
+		boolean comp4 = s2.compareImmutableStrings(s2, s3);
+		boolean comp5 = s2.compareImmutableStrings(s2, s4);
+		boolean comp6 = s2.compareImmutableStrings(s3, s4);
+
+		System.out.println("Test " + s1.getString() + ", " + s2.getString() + ": " + comp1);
+		System.out.println("Test " + s1.getString() + ", " + s3.getString() + ": " + comp2);
+		System.out.println("Test " + s1.getString() + ", " + s4.getString() + ": " + comp3);
+		System.out.println("Test " + s2.getString() + ", " + s3.getString() + ": " + comp4);
+		System.out.println("Test " + s2.getString() + ", " + s4.getString() + ": " + comp5);
+		System.out.println("Test " + s3.getString() + ", " + s4.getString() + ": " + comp6);
+
+
 	}
 }
--- src/immutableClass/immutableString.java
+++ src/immutableClass/immutableString.java
@@ -9,5 +9,15 @@ public final class immutableString {
 	public String getString() {
 		return string;
 	}
-
+	
+	public boolean compareImmutableStrings(immutableString s1, immutableString s2) {
+		String string1 = s1.getString();
+		String string2 = s2.getString();
+		if (string1.length() == string2.length()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+ 
 }
