12dd8347b1a62002a3a5cca6c2d8162f0bd1e239
Thomas Meyer <thomasmmeyer88@gmail.com>
2021-07-11 15:14:56 -0500

    immutable class

--- src/immutableClass/immutableString.java
+++ src/immutableClass/immutableString.java
@@ -1,5 +1,13 @@
 package immutableClass;
 
 public final class immutableString {
+	private String string;
+
+	public immutableString(String string) {
+		this.string = string;
+	}
+	public String getString() {
+		return string;
+	}
 
 }
